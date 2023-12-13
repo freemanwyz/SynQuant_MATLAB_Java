029f4190dad6f895b3402514f75608ee572ebadb
Yizhi Wang <yzwang@vt.edu>
2023-12-13 13:10:34 -0500

    Example with two channels

---
+++ synquant_3d_two_channels.m
@@ -0,0 +1,51 @@
+function [outMap,xVol] = synquant_3d_two_channels(datIn,xstd,minSz,maxSz)
+    
+    % run each channel
+    xVol = cell(2,1);
+    for kk=1:numel(datIn)
+        imgIn = datIn{kk};
+        xVol{kk} = synquant_3d_single(imgIn,xstd,minSz,maxSz);
+    end
+    
+    % update the score for each post-synaptic puncta
+    dext = 0;  % search in XY
+    dextz = 0;  % search in Z
+    
+    cc = bwconncomp(xVol{2});
+    sLst = cc.PixelIdxList;  % check each post puncta
+    postVol = xVol{2};
+    preVol = xVol{1};
+    
+    [H1,W1,D1] = size(datIn{1});
+    outMap = zeros(H1,W1,D1);
+    for ii=1:numel(sLst)
+        s0 = sLst{ii};
+        [h0,w0,d0] = ind2sub([H1,W1,D1],s0);
+        zPost = mean(postVol(s0));
+        
+        % find highest pre puncta score
+        zPre = 0;
+        for jj=1:numel(h0)
+            h00 = h0(jj); w00 = w0(jj); d00 = d0(jj);
+            hrg = max(h00-dext,1):min(h00+dext,H1);
+            wrg = max(w00-dext,1):min(w00+dext,W1);
+            drg = max(d00-dextz,1):min(d00+dextz,D1);
+            preVolCrop = preVol(hrg,wrg,drg);
+            zPre = max(max(preVolCrop(:)),zPre);
+        end
+        
+        % combine scores
+        outMap(s0) = zPost+zPre;
+    end
+    
+end
+
+
+
+
+
+
+
+
+
+
